3b0840b1cc3a1ad70446db1fdf92d61d6a6965cf
sheamuspy <sheamuspy@gmail.com>
2015-11-24 15:34:25 +0000

    Add to cart added

--- app/src/main/java/com/adziban/adziban/customer/Home.java
+++ app/src/main/java/com/adziban/adziban/customer/Home.java
@@ -46,8 +46,13 @@ public class Home extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Order Placed", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
+
+
+
             }
         });
 
